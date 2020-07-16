7916712cb6eef2da4358d507415be84eb0f10a7e
jerryfane <38183696+jerryfane@users.noreply.github.com>
2020-07-16 17:13:44 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,2 +1,10 @@
 buy_eur_per_day = 4.00
 btc_to_buy = 0
+
+#set true if you want to get the amount of BTC to buy from the amount saved on data.csv
+continue_from_last_day = True
+
+
+if continue_from_last_day:
+    data = pd.read_csv('./data.csv')
+    btc_to_buy = data['total_btc'].values[-1]
