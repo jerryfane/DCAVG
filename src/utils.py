6cf233bad3644510d9abbb2b4d75676c922d841b
jerryfane <38183696+jerryfane@users.noreply.github.com>
2020-07-16 17:30:01 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -70,4 +70,10 @@ def is_time_between(begin_time, end_time, check_time=None):
         return check_time >= begin_time and check_time <= end_time
     else: # crosses midnight
         return check_time >= begin_time or check_time <= end_time
+    
+def line_prepender(filename, line):
+    with open(filename, 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write(line.rstrip('\r\n') + '\n' + content)
 
